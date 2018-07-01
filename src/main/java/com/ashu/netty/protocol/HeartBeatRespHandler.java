89d46305279cbc5b154277167bacd3526041ca92
ashu <ithinker1991@gmail.com>
2018-07-01 10:47:03 +0800

    add heartbeat handler

---
+++ src/main/java/com/ashu/netty/protocol/HeartBeatRespHandler.java
@@ -0,0 +1,35 @@
+package com.ashu.netty.protocol;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class HeartBeatRespHandler extends ChannelInboundHandlerAdapter {
+
+  @Override
+  public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    NettyMessage message = (NettyMessage)msg;
+    if (message.getHeader() != null && message.getHeader().getType() == MessageType.HEARTBEAT.asByte()) {
+
+      NettyMessage heartBeatResp = buildHeartResp();
+      ctx.writeAndFlush(heartBeatResp);
+
+    } else {
+      ctx.fireChannelRead(msg);
+    }
+  }
+
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    ctx.fireExceptionCaught(cause);
+  }
+
+  private NettyMessage buildHeartResp() {
+    NettyMessage message = new NettyMessage();
+    Header header = new Header();
+    header.setType(MessageType.HEARTBEAT_RESP.asByte());
+    message.setHeader(header);
+    return message;
+  }
+
+
+}
--- src/main/java/com/ashu/netty/protocol/MessageType.java
+++ src/main/java/com/ashu/netty/protocol/MessageType.java
@@ -7,7 +7,9 @@ import java.util.Map;
 public enum MessageType {
   FIRST(0x00),
   LOGIN(0x01),
-  LOGIN_RESP(0x02);
+  LOGIN_RESP(0x02),
+  HEARTBEAT(0x03),
+  HEARTBEAT_RESP(0x04);
 
 //  TRX(0x01),
 //
