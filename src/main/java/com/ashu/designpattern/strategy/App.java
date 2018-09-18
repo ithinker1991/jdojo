4d6ab6145d4b69a18b45eb1fcc063e35c3c80666
ashu <yinshucheng@xiaomi.com>
2018-09-19 00:17:14 +0800

    polish

--- src/main/java/com/ashu/designpattern/strategy/App.java
+++ src/main/java/com/ashu/designpattern/strategy/App.java
@@ -1,9 +1,5 @@
 package com.ashu.designpattern.strategy;
 
-import com.google.common.collect.Lists;
-
-import java.util.List;
-
 public class App {
     public static void main(String[] args) {
         Duck[] ducks = {new RubberDuck(), new RubberDuck()};
